9820de9908bb10f518ae595083925d6afe4a4755
ly <microly@126.com>
2020-04-04 13:12:44 +0800

    [ggpolar.R] create file

---
+++ R/ggpolar.R
@@ -0,0 +1,10 @@
+#' A ggplot2 Extension to Plot Data in Polar Coordinates on a Cartesian Layer
+#'
+#' This package supplies a few ggplot2 facilities to plot data in polar coordinates
+#'  on a cartesian layer. For more complete documentation, read the vignettes.
+#'
+#' @docType package
+#' @name ggpolar
+#'
+#' @importFrom magrittr %>%
+NULL
